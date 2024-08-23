080cfbd6b83a52af25dd502281a53926dffb6296
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-23 13:54:14 +0530

    display chat messages in the DOM

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,10 @@ const chatForm = document.getElementById('chat-form');
 
 const socket = io();
 
+// Message from server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
 });
 
 // Message submit
@@ -17,3 +19,14 @@ chatForm.addEventListener('submit', (e) => {
     socket.emit('chatMessage', message);
 
 });
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = (`<p class="meta">Dinuka <span>9:12pm</span></p>
+        <p class="text">
+            ${message}
+        </p>`);
+    document.querySelector('.chat-messages').appendChild(div);
+}
